fc69e15b57417e27a118dcc90de5af821037853e
Ahmad Jarara <ajarara94@gmail.com>
2018-04-07 14:50:49 -0400

    add apply.sh which applies changes to current env

---
+++ apply.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+
+echo "Overwriting ~/.config with files from ~/dotfiles/config"
+rsync -av ~/dotfiles/.config ~/ > /dev/null
+echo "Clobbering x250's bash config"
+rsync -av ~/dotfiles/x250/.bash_profile ~ > /dev/null
+rsync -av ~/dotfiles/x250/.bashrc ~ > /dev/null
+echo "Clobber .nixpkgs/config.nix"
+rsync -av ~/dotfiles/config.nix ~/.nixpkgs/config.nix > /dev/null
+echo "Installing declared packages in ~/dotfiles/.nixpkgs/config.nix"
+nix-env -i all
+
+nix-env -f ~/dotfiles/default.nix
+echo "Done!"
