295d3f51cf6370e12828c2afd08a0ac71f4c246f
Ahmad Jarara <ajarara94@gmail.com>
2019-10-21 21:40:23 -0400

    use ssh always for github

--- apply.sh
+++ apply.sh
@@ -5,4 +5,5 @@ rsync -av ~/dotfiles/.config ~/ > /dev/null
 echo "Clobbering x250's bash config"
 rsync -av ~/dotfiles/x250/.bash_profile ~ > /dev/null
 rsync -av ~/dotfiles/x250/.bashrc ~ > /dev/null
+rsync -av ~/dotfiles/.gitconfig ~ > /dev/null
 echo "Done!"
