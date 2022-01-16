9a3529e2ed67beb7a8d3607be0a0f61ded2babf6
Ahmad Jarara <git@ajarara.io>
2022-01-15 21:34:46 -0500

    Drop kfreestyle2.sh

--- kfreestyle2.sh
+++
@@ -1,6 +0,0 @@
-
-# rather than use something like kfreestyle2d, I'm fine with giving up C-f[8-10] for convenience.
-# but I can never remember what to send, so I might as well alias them here. 
-alias mute='xdotool key "XF86AudioMute"'
-alias louder='xdotool key "XF86AudioRaiseVolume"'
-alias quieter='xdotool key "XF86AudioLowerVolume"'
